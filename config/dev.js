941d31e7707cd5ecc198e6c7d505e98106327b35
vikashsurin <vikashsurin10@gmail.com>
2019-08-09 06:17:43 +0530

    initial commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientId:
+    '722902472935-8a7ob29ski0d1jjfc6vc8tgrdcimojbe.apps.googleusercontent.com',
+  googleClientSecret: 'v8IZmDSoRg9VM4NkXkDGT9uZ',
+  mongoURI:
+    'mongodb+srv://vikash:vikash@cluster0-mnbrc.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: 'sdfhsdfhjksdfhkljshflasdfjksafdk'
+};
---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
+mongoose.connection
+  .on('error', console.error.bind(console, 'connection error'))
+  .once('open', () => {
+    console.log('connection established to mongoDB');
+  });
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`the server is running at port ${PORT}`);
+});
